4851830f393dd9b878c51617c909ad8ccaf6547a
jwallace22 <47610227+jwallace22@users.noreply.github.com>
2019-11-11 17:01:00 -0600

    comments added. final commit.

--- card.cpp
+++ card.cpp
@@ -6,28 +6,28 @@
 #include <iostream>
 using namespace std;
 Card::Card() {
-    myRank = 1;
+    myRank = 1; //the default card is an Ace of spades
     mySuit = spades;
 }
 
 Card::Card(int rank, Card::Suit s) {
-    myRank = rank;
+    myRank = rank; //initialize the card with the parameters passed
     mySuit = s;
 }
 
 string Card::toString() const {
     string suit = suitString(mySuit);
     string rank = rankString(myRank);
-    return(rank + suit);
+    return(rank + suit); //the 2 strings are added to create a 2 character string such as As for ace of spades
 }
 
 bool Card::sameSuitAs(const Card &c) const {
-    return(mySuit == c.getSuit());
+    return(mySuit == c.getSuit()); 
 }
 
 string Card::suitString(Card::Suit s) const {
     string suit;
-    switch(s){
+    switch(s){ //switch statement based on the input suit since the types were enumerated and finite
         case spades: suit = "s";
             break;
         case hearts: suit = "h";
@@ -55,7 +55,7 @@ string Card::rankString(int r) const {
             break;
         case 3: rank = "3";
             break;
-        case 4: rank = "4";
+        case 4: rank = "4"; // this switch statement was used because stoi was giving us issues on linux. 
             break;
         case 5: rank = "5";
             break;
